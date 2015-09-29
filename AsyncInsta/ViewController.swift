d0f3a7cc92e5a74d8a1dbac84cf39b89eca84138
John Regner <reg>
2015-09-28 20:44:58 -0700

    mend

--- AsyncInsta/ViewController.swift
+++ AsyncInsta/ViewController.swift
@@ -5,18 +5,36 @@ import SafariServices
 
 import UIKit
 
-class ViewController: UIViewController {
+
+let loginURL = "https://api.instagram.com/oauth/authorize/?client_id=c10d736ee85e4d2aa6b44e7032a98009&redirect_uri=https://johnregner.com/AsyncInsta&response_type=code"
+
+class ViewController: UIViewController, SFSafariViewControllerDelegate {
+
+    @IBOutlet var loginButton: UIButton!
+
+    let url = NSURL(string: loginURL)!
+    lazy var svc: SFSafariViewController = { return SFSafariViewController(URL: self.url)}()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: ":safariLogin:", name: Notifications.CloseSafariViewController.rawValue, object: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func safariLogin(notification: NSNotification){
+
+        let url = notification.object as! NSURL
+        print("GOT URL: \(url)")
+        self.svc.dismissViewControllerAnimated(true, completion: {print("dismissed SVC in Notification Handler")})
     }
 
 
+    @IBAction func showLoginVC(sender:UIButton){
+        svc.delegate = self
+        presentViewController(svc, animated: true, completion: {print("Completed showing Safari View Controller")})
+    }
+
+    func safariViewControllerDidFinish(controller: SFSafariViewController) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
